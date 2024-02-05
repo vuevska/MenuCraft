716137204d5dc17f3267b664f9fe79c41b2910a5
David <arsovskidavid4@gmail.com>
2024-02-06 00:37:30 +0100

    login izgled i animacii (ce gi smenime sigurno)

--- lib/pages/authentication/sign_up.dart
+++ lib/pages/authentication/sign_up.dart
@@ -1,3 +1,4 @@
+import 'package:animate_do/animate_do.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:menu_craft/utils/toastification.dart';
@@ -23,82 +24,123 @@ class _SignUpState extends State<SignUp> {
   Widget build(BuildContext context) {
     return Scaffold(
         //TODO:  da se sredi malce izgledov
+        backgroundColor: Theme.of(context).primaryColor,
         body: Container(
-      color: Theme.of(context).primaryColor,
-      child: Container(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          children: [
-            textInputAuth(
-                label: "Email",
-                controller: _emailController,
-                icon: Icons.email,
-                pass: false),
-            textInputAuth(
-                label: "Name",
-                controller: _nameController,
-                icon: Icons.account_box,
-                pass: false),
-            textInputAuth(
-                label: "Surname",
-                controller: _surnameController,
-                icon: Icons.account_box,
-                pass: false),
-            textInputAuth(
-                label: "Password",
-                controller: _passController,
-                icon: Icons.password,
-                pass: true),
-            textInputAuth(
-                label: "Confirm Password",
-                controller: _passConfirmController,
-                icon: Icons.password,
-                pass: true),
-            Container(
-              width: double.infinity,
-              padding: const EdgeInsets.all(8.0),
-              child: ElevatedButton(
-                onPressed: () {
-                  if (_passController.text == _passConfirmController.text) {
-                    AuthService.signUpWithMail(
-                      context,
-                      _emailController.text,
-                      _passController.text,
-                      _nameController.text,
-                      _surnameController.text,
-                    ).then((value) {
-                      if (value != null) {
-                        ToastificationUtil.show(
-                            context, "Please verify your email address");
-                        Navigator.maybePop(context);
+          padding: const EdgeInsets.all(16.0),
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                const SizedBox(
+                  height: 30,
+                ),
+                BounceInDown(
+                  from: 25,
+                  duration: const Duration(milliseconds: 700),
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(20.0),
+                    child: Image(
+                      image: const AssetImage('images/signUpImage.jpg'),
+                      width: MediaQuery.of(context).size.width,
+                      height: 200,
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                ),
+                FadeInDown(
+                  duration: const Duration(milliseconds: 200),
+                  from: 25,
+                  child: const Padding(
+                    padding: EdgeInsets.all(8.0),
+                    child: Text("Sign Up",
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 48,
+                            fontWeight: FontWeight.bold),
+                        textAlign: TextAlign.left),
+                  ),
+                ),
+                textInputAuth(
+                    label: "Email",
+                    controller: _emailController,
+                    icon: Icons.email,
+                    pass: false),
+                textInputAuth(
+                    label: "Name",
+                    controller: _nameController,
+                    icon: Icons.account_box,
+                    pass: false),
+                textInputAuth(
+                    label: "Surname",
+                    controller: _surnameController,
+                    icon: Icons.account_box,
+                    pass: false),
+                textInputAuth(
+                    label: "Password",
+                    controller: _passController,
+                    icon: Icons.password,
+                    pass: true),
+                textInputAuth(
+                    label: "Confirm Password",
+                    controller: _passConfirmController,
+                    icon: Icons.password,
+                    pass: true),
+                Container(
+                  width: double.infinity,
+                  padding: const EdgeInsets.all(8.0),
+                  child: ElevatedButton(
+                    onPressed: () {
+                      if (_passController.text == _passConfirmController.text) {
+                        AuthService.signUpWithMail(
+                          context,
+                          _emailController.text,
+                          _passController.text,
+                          _nameController.text,
+                          _surnameController.text,
+                        ).then((value) {
+                          if (value != null) {
+                            ToastificationUtil.show(
+                                context, "Please verify your email address");
+                            Navigator.maybePop(context);
+                          }
+                        }).catchError((onError) {
+                          onError as FirebaseAuthException;
+
+                          ScaffoldMessenger.of(context).showSnackBar(
+                            SnackBar(content: Text(onError.message ?? "")),
+                          );
+                        });
                       }
-                    }).catchError((onError){
-
-                      onError as FirebaseAuthException;
-
-                      ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-                          content: Text(onError.message ?? "" )),
-                      );
-                    });
-                  } //TODO: Add error handling
-                  //TODO: Add password strength check
-                },
-                child: const Text('Sign Up'),
-              ),
+                      //TODO: Add password strength check
+                    },
+                    child: const Text(
+                      'Sign Up',
+                      style: TextStyle(
+                        fontSize: 24,
+                      ),
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  width: double.infinity,
+                  child: TextButton(
+                    onPressed: () {
+                      Navigator.maybePop(context);
+                    },
+                    style: ButtonStyle(
+                      foregroundColor: MaterialStateProperty.all(Colors.grey),
+                      textStyle: MaterialStateProperty.all(
+                        const TextStyle(
+                          fontSize: 18,
+                        ),
+                      ),
+                    ),
+                    child: const Text("You already have an account? Log in!"),
+                  ),
+                )
+              ],
             ),
-            SizedBox(
-              width: double.infinity,
-              child: ElevatedButton(
-                onPressed: () {
-                  Navigator.maybePop(context);
-                },
-                child: const Text("Log In"),
-              ),
-            )
-          ],
-        ),
-      ),
-    ));
+          ),
+        ));
   }
 
   @override
--- lib/widgets/authentication/input_auth.dart
+++ lib/widgets/authentication/input_auth.dart
@@ -1,3 +1,4 @@
+import 'package:animate_do/animate_do.dart';
 import 'package:flutter/material.dart';
 
 Widget textInputAuth({
@@ -6,22 +7,26 @@ Widget textInputAuth({
   required IconData icon,
   required bool pass,
 }) {
-  return TextFormField(
-    controller: controller,
-    decoration: InputDecoration(
-      icon: Icon(icon),
-      iconColor: Colors.white,
-      labelText: label,
-      labelStyle: const TextStyle(
+  return FadeInDown(
+    duration: const Duration(milliseconds: 300),
+    from:50,
+    child: TextFormField(
+      controller: controller,
+      decoration: InputDecoration(
+        icon: Icon(icon),
+        iconColor: Colors.white,
+        labelText: label,
+        labelStyle: const TextStyle(
+          color: Colors.white,
+        ),
+      ),
+      cursorColor: Colors.white,
+      style: const TextStyle(
         color: Colors.white,
       ),
+      obscureText: pass,
+      enableSuggestions: !pass,
+      autocorrect: !pass,
     ),
-    cursorColor: Colors.white,
-    style: const TextStyle(
-      color: Colors.white,
-    ),
-    obscureText: pass,
-    enableSuggestions: !pass,
-    autocorrect: !pass,
   );
 }
