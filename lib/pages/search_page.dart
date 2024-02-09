632b5270f75313b71f3a73f41bb4ee80dc5b62b1
Maria <saveska.maria@gmail.com>
2024-02-09 04:06:45 +0100

    Changed search bar UI

--- lib/pages/search_page.dart
+++ lib/pages/search_page.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:menu_craft/widgets/appbar/custom_appbar.dart';
 import 'package:menu_craft/widgets/search_page/search_input.dart';
+import '../../models/restaurant_model.dart'; // Import your RestaurantModel
+import '../../services/db_service.dart';
 
 class SearchPage extends StatelessWidget {
   const SearchPage({super.key});
@@ -21,3 +23,53 @@ class SearchPage extends StatelessWidget {
     );
   }
 }
+
+// class SearchPage extends StatefulWidget {
+//   const SearchPage({Key? key});
+//
+//   @override
+//   _SearchPageState createState() => _SearchPageState();
+// }
+//
+// class _SearchPageState extends State<SearchPage> {
+//   late Future<List<RestaurantModel>> _restaurantsFuture;
+//
+//   @override
+//   void initState() {
+//     super.initState();
+//     _restaurantsFuture = DbAuthService().getAllRestaurants(); // Fetch list of restaurants from database
+//   }
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: const CustomAppBar(),
+//       body: Container(
+//         padding: const EdgeInsets.all(16.0),
+//         color: Theme.of(context).primaryColor,
+//         child: FutureBuilder<List<RestaurantModel>>(
+//           future: _restaurantsFuture,
+//           builder: (context, snapshot) {
+//             if (snapshot.connectionState == ConnectionState.waiting) {
+//               return const Center(
+//                 child: CircularProgressIndicator(),
+//               );
+//             } else if (snapshot.hasError) {
+//               return Center(
+//                 child: Text('Error: ${snapshot.error}'),
+//               );
+//             } else {
+//               List<RestaurantModel> restaurants = snapshot.data ?? [];
+//               return Column(
+//                 children: [
+//                   SearchBarCustom(restaurants: restaurants), // Pass the list of restaurants to SearchBarCustom
+//                   // Other widgets...
+//                 ],
+//               );
+//             }
+//           },
+//         ),
+//       ),
+//     );
+//   }
+// }
--- lib/widgets/search_page/search_input.dart
+++ lib/widgets/search_page/search_input.dart
@@ -1,46 +1,140 @@
-
 import 'package:flutter/material.dart';
+import 'package:menu_craft/models/restaurant_model.dart';
+import '../appbar/custom_appbar.dart';
+
+// class SearchBarCustom extends StatefulWidget {
+//   const SearchBarCustom({super.key});
+//
+//   @override
+//   State<SearchBarCustom> createState() => _SearchBarCustomState();
+// }
+//
+// class _SearchBarCustomState extends State<SearchBarCustom> {
+//   @override
+//   Widget build(BuildContext context) {
+//     return SearchAnchor(
+//     builder: (BuildContext context, SearchController controller) {
+//       return SearchBar(
+//         controller: controller,
+//         padding: const MaterialStatePropertyAll<EdgeInsets>(
+//             EdgeInsets.symmetric(horizontal: 16.0)),
+//         onTap: () {
+//           controller.openView();
+//         },
+//         onChanged: (_) {
+//           controller.openView();
+//         },
+//         leading: const Icon(Icons.search),
+//         // trailing: <Widget>[
+//         // ],
+//       );
+//     },
+//     suggestionsBuilder: (BuildContext context,
+//         SearchController controller) {
+//       return List<ListTile>.generate(5, (int index) {
+//         final String item = 'item $index';
+//         return ListTile(
+//           title: Text(item),
+//           onTap: () {
+//             setState(() {
+//               controller.closeView(item);
+//             });
+//           },
+//         );
+//       });
+//     });
+//   }
+// }
+
 
 class SearchBarCustom extends StatefulWidget {
-  const SearchBarCustom({super.key});
+  const SearchBarCustom({Key? key});
 
   @override
   State<SearchBarCustom> createState() => _SearchBarCustomState();
 }
 
 class _SearchBarCustomState extends State<SearchBarCustom> {
+  bool _isListVisible = false;
+  late FocusNode _focusNode;
+
+  @override
+  void initState() {
+    super.initState();
+    _focusNode = FocusNode();
+  }
+
+  @override
+  void dispose() {
+    _focusNode.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return SearchAnchor(
-    builder: (BuildContext context, SearchController controller) {
-      return SearchBar(
-        controller: controller,
-        padding: const MaterialStatePropertyAll<EdgeInsets>(
-            EdgeInsets.symmetric(horizontal: 16.0)),
-        onTap: () {
-          controller.openView();
-        },
-        onChanged: (_) {
-          controller.openView();
-        },
-        leading: const Icon(Icons.search),
-        // trailing: <Widget>[
-        // ],
-      );
-    },
-    suggestionsBuilder: (BuildContext context,
-        SearchController controller) {
-      return List<ListTile>.generate(5, (int index) {
-        final String item = 'item $index';
-        return ListTile(
-          title: Text(item),
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        GestureDetector(
           onTap: () {
             setState(() {
-              controller.closeView(item);
+              _isListVisible = !_isListVisible;
             });
+            if (_isListVisible) {
+              Future.delayed(Duration(milliseconds: 100), () {
+                FocusScope.of(context).requestFocus(_focusNode);
+              });
+            }
           },
-        );
-      });
-    });
+          child: Container(
+            padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 10.0),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(20.0), // Adjust the value to change the roundness
+              color: Colors.purple.shade50,
+            ),
+            child: Row(
+              children: const [
+                Icon(Icons.search),
+                SizedBox(width: 8.0),
+                Text('Search...'),
+              ],
+            ),
+          ),
+        ),
+        SizedBox(height: 8.0),
+        Visibility(
+          visible: _isListVisible,
+          child: Container(
+            height: 300, // Adjust height according to your need
+            padding: const EdgeInsets.symmetric(horizontal: 16.0,vertical: 5.0),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(20.0), // Adjust the value to change the roundness
+              color: Colors.purple.shade50,
+            ),
+            child: ListView.builder(
+              itemCount: 10, // Adjust itemCount according to your list
+              itemBuilder: (context, index) {
+                return ListTile(
+                  title: Text('Item $index'),
+                  onTap: () {
+                    // Handle item tap
+                  },
+                );
+              },
+            ),
+          ),
+        ),
+        // Hidden text field used for opening the keyboard
+        // when the search bar is tapped
+        Opacity(
+          opacity: 0.0,
+          child: TextField(
+            focusNode: _focusNode,
+          ),
+        ),
+      ],
+    );
   }
 }
+
+
