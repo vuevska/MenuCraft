75fb02166f24f43e009dff31151098a6f1662791
Maria <saveska.maria@gmail.com>
2024-02-11 00:28:54 +0100

    Privacy Policy Page

--- lib/widgets/profile/settings/privacy_policy/category_text.dart
+++ lib/widgets/profile/settings/privacy_policy/category_text.dart
@@ -72,7 +72,7 @@ While using Our Service, We may ask You to provide Us with certain personally id
 
       case 'Usage Data':
         return
-          'Usage Data is collected automatically when using the Service.'
+          'Usage Data is collected automatically when using the Service.\n\n'
           'When You access the Service by or through a mobile device, '
           'We may collect certain information automatically, including, '
           'but not limited to, the type of mobile device You use, Your mobile device '
@@ -80,25 +80,78 @@ While using Our Service, We may ask You to provide Us with certain personally id
           'unique device identifiers and other diagnostic data';
 
       case 'Information from Third-Party Social Media Services':
-        return 'Text';
+        return '''
+The Company allows You to create an account and log in to use the Service through the following Third-party Social Media Services:
+
+  - Google
+  - Facebook
+  - Instagram
+  - Twitter
+  - LinkedIn
+  
+If You decide to register through or otherwise grant us access to a Third-Party Social Media Service, We may collect Personal data that is already associated with Your Third-Party Social Media Service's account, such as Your name, Your email address, Your activities or Your contact list associated with that account.
+
+You may also have the option of sharing additional information with the Company through Your Third-Party Social Media Service's account. If You choose to provide such information and Personal Data, during registration or otherwise, You are giving the Company permission to use, share, and store it in a manner consistent with this Privacy Policy.''';
 
       case 'Information Collected while Using the Application':
-        return 'Text';
+        return '''
+While using Our Application, in order to provide features of Our Application, We may collect, with Your prior permission:
+
+Information regarding your location
+
+Pictures and other information from your Device's camera and photo library
+
+We use this information to provide features of Our Service, to improve and customize Our Service. The information may be uploaded to the Company's servers and/or a Service Provider's server or it may be simply stored on Your device.
+
+You can enable or disable access to this information at any time, through Your Device settings.''';
 
       case 'Use of Your Personal Data':
-        return 'Text';
+        return '''
+The Company may use Personal Data for the following purposes:
+
+  - To provide and maintain our Service, including to monitor the usage of our Service.
+
+  - To manage Your Account: to manage Your registration as a user of the Service. The Personal Data You provide can give You access to different functionalities of the Service that are available to You as a registered user.
+
+  - For the performance of a contract: the development, compliance and undertaking of the purchase contract for the products, items or services You have purchased or of any other contract with Us through the Service.
+
+  - To contact You: To contact You by email, telephone calls, SMS, or other equivalent forms of electronic communication, such as a mobile application's push notifications regarding updates or informative communications related to the functionalities, products or contracted services, including the security updates, when necessary or reasonable for their implementation.
+
+  - To provide You with news, special offers and general information about other goods, services and events which we offer that are similar to those that you have already purchased or enquired about unless You have opted not to receive such information.
+
+  - To manage Your requests: To attend and manage Your requests to Us.
+
+  - For business transfers: We may use Your information to evaluate or conduct a merger, divestiture, restructuring, reorganization, dissolution, or other sale or transfer of some or all of Our assets, whether as a going concern or as part of bankruptcy, liquidation, or similar proceeding, in which Personal Data held by Us about our Service users is among the assets transferred.
+
+  - For other purposes: We may use Your information for other purposes, such as data analysis, identifying usage trends, determining the effectiveness of our promotional campaigns and to evaluate and improve our Service, products, services, marketing and your experience.''';
 
       case 'Retention of Your Personal Data':
-        return 'Text';
+        return '''
+The Company will retain Your Personal Data only for as long as is necessary for the purposes set out in this Privacy Policy. We will retain and use Your Personal Data to the extent necessary to comply with our legal obligations (for example, if we are required to retain your data to comply with applicable laws), resolve disputes, and enforce our legal agreements and policies.
+
+The Company will also retain Usage Data for internal analysis purposes. Usage Data is generally retained for a shorter period of time, except when this data is used to strengthen the security or to improve the functionality of Our Service, or We are legally obligated to retain this data for longer time periods.''';
 
       case 'Delete Your Personal Data':
-        return 'Text';
+        return '''
+You have the right to delete or request that We assist in deleting the Personal Data that We have collected about You.
+
+Our Service may give You the ability to delete certain information about You from within the Service.
+
+You may update, amend, or delete Your information at any time by signing in to Your Account, if you have one, and visiting the account settings section that allows you to manage Your personal information. You may also contact Us to request access to, correct, or delete any personal information that You have provided to Us.
+
+Please note, however, that We may need to retain certain information when we have a legal obligation or lawful basis to do so.''';
 
       case 'Security of Your Personal Data':
-        return 'Text';
+        return '''
+The security of Your Personal Data is important to Us, but remember that no method of transmission over the Internet, or method of electronic storage is 100% secure. While We strive to use commercially acceptable means to protect Your Personal Data, We cannot guarantee its absolute security.''';
 
       case 'Changes to this Privacy Policy':
-        return 'Text';
+        return '''
+We may update Our Privacy Policy from time to time. We will notify You of any changes by posting the new Privacy Policy on this page.
+
+We will let You know via email and/or a prominent notice on Our Service, prior to the change becoming effective and update the "Last updated" date at the top of this Privacy Policy.
+
+You are advised to review this Privacy Policy periodically for any changes. Changes to this Privacy Policy are effective when they are posted on this page.''';
 
       default:
         return '';
