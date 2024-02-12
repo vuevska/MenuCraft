4b86dab8b59ec300a1f452043bb10aeb2687b8b1
Maria <saveska.maria@gmail.com>
2024-02-12 04:07:36 +0100

    Updated UI

--- lib/widgets/home_page/restaurant_categories/restaurant_category_grid.dart
+++ lib/widgets/home_page/restaurant_categories/restaurant_category_grid.dart
@@ -24,8 +24,8 @@ class CategoryGrid extends StatelessWidget {
           return GridView.builder(
             gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
               crossAxisCount: 4,
-              mainAxisSpacing: 10.0,
-              crossAxisSpacing: 10.0,
+              mainAxisSpacing: 8.0,
+              crossAxisSpacing: 8.0,
               childAspectRatio: 1.0,
             ),
             itemCount: categories.length,
