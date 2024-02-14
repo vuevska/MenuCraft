59ebabe9228d6ed9565d54372ef784c1f01285c4
David <arsovskidavid4@gmail.com>
2024-02-14 01:05:20 +0100

    main menu animations & favorites fix & qr code back button fix

--- lib/pages/favourites_page.dart
+++ lib/pages/favourites_page.dart
@@ -21,45 +21,51 @@ class _FavoritesPageState extends State<FavoritesPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: const CustomAppBar(),
-      body: Container(
-        color: Theme.of(context).primaryColor,
-        child: Padding(
-          padding: const EdgeInsets.only(top: 22.0),
-          child: Consumer<UserProvider>(
-            builder: (context, userProvider, child) {
-              // Access the user's favorite restaurants from the user provider
+      body: RefreshIndicator(
+        onRefresh: () async {
+          setState(() {});
+        },
+        child: Container(
+          color: Theme.of(context).primaryColor,
+          child: Padding(
+            padding: const EdgeInsets.only(top: 22.0),
+            child: Consumer<UserProvider>(
+              builder: (context, userProvider, child) {
+                // Access the user's favorite restaurants from the user provider
 
-              return Consumer<FavoriteProvider>(
-                  builder: (context, favoriteProvider, child) {
-                return FutureBuilder(
-                    future: context
-                        .read<FavoriteProvider>()
-                        .getFavorites(AuthService.user?.uid ?? 'local'),
-                    builder: (context, snapshot) {
-                      if (snapshot.connectionState == ConnectionState.waiting) {
-                        return const Center(
-                          child: CircularProgressIndicator(
-                            color: Colors.white,
-                          ),
-                        );
-                      }
-                      if (snapshot.hasError) {
-                        return const Center(
-                          child: Text('An error occurred'),
-                        );
-                      }
-                      final favoriteRestaurants =
-                          snapshot.data as List<RestaurantModel>;
-                      if (favoriteRestaurants.isEmpty) {
-                        return const Center(
-                          child: Text('No favorite restaurants'),
-                        );
-                      }
-                      return FavoriteList(
-                          favoriteRestaurants: favoriteRestaurants);
-                    });
-              });
-            },
+                return Consumer<FavoriteProvider>(
+                    builder: (context, favoriteProvider, child) {
+                  return FutureBuilder(
+                      future: context
+                          .read<FavoriteProvider>()
+                          .getFavorites(AuthService.user?.uid ?? 'local'),
+                      builder: (context, snapshot) {
+                        if (snapshot.connectionState == ConnectionState.waiting) {
+                          return const Center(
+                            child: CircularProgressIndicator(
+                              color: Colors.white,
+                            ),
+                          );
+                        }
+                        if (snapshot.hasError) {
+
+                          return const Center(
+                            child: Text('An error occurred', style: TextStyle(color: Colors.white)),
+                          );
+                        }
+                        final favoriteRestaurants =
+                            snapshot.data as List<RestaurantModel>;
+                        if (favoriteRestaurants.isEmpty) {
+                          return const Center(
+                            child: Text('No favorite restaurants'),
+                          );
+                        }
+                        return FavoriteList(
+                            favoriteRestaurants: favoriteRestaurants);
+                      });
+                });
+              },
+            ),
           ),
         ),
       ),
--- lib/utils/generate_qr.dart
+++ lib/utils/generate_qr.dart
@@ -26,44 +26,43 @@ class QRGenOverlay {
   OverlayEntry _createOverlay() {
     return OverlayEntry(
       builder: (BuildContext context) {
-        return Positioned(
-          top: MediaQuery.of(context).size.height *
-              0.15, // Adjust position as needed
-          left: MediaQuery.of(context).size.width *
-              0.1, // Adjust position as needed
-          child: Material(
-            color: Colors.transparent,
-            child: SizedBox(
-              width: MediaQuery.of(context).size.width *
-                  0.8, // Adjust size as needed
-              height: MediaQuery.of(context).size.height *
-                  0.6, // Adjust size as needed
+        return Scaffold(
+          backgroundColor: Colors.black54,
+          body: Center(
+            child: Material(
+              color: Colors.transparent,
+              child: SizedBox(
+                width: MediaQuery.of(context).size.width *
+                    0.8, // Adjust size as needed
+                height: MediaQuery.of(context).size.height *
+                    0.6, // Adjust size as needed
 
-              child: Container(
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.only(bottomLeft: Radius.circular(16.0),bottomRight: Radius.circular(16.0)),
-                  color: const Color.fromRGBO(60, 45, 68, 1.0),
-                ),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-                    QrImageView(
-                      data: data,
-                      size: MediaQuery.of(context).size.width * 0.70,
-                      padding: const EdgeInsets.all(30),
-                      backgroundColor: Colors.white,
-                      embeddedImage: AssetImage('images/logo.jpg'),
+                child: Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.only(bottomLeft: Radius.circular(16.0),bottomRight: Radius.circular(16.0)),
+                    color: const Color.fromRGBO(60, 45, 68, 1.0),
+                  ),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      QrImageView(
+                        data: data,
+                        size: MediaQuery.of(context).size.width * 0.70,
+                        padding: const EdgeInsets.all(30),
+                        backgroundColor: Colors.white,
+                        embeddedImage: AssetImage('images/logo.jpg'),
 
-                    ),
-                    Text(restaurantName, style: TextStyle(color: Colors.white, fontSize: 48, fontWeight: FontWeight.bold)), //TODO: ovde eden jak font i prejako ce lici
-                    SizedBox(height: 20),
-                    ElevatedButton(
+                      ),
+                      Text(restaurantName, style: TextStyle(color: Colors.white, fontSize: 48, fontWeight: FontWeight.bold)), //TODO: ovde eden jak font i prejako ce lici
+                      SizedBox(height: 20),
+                      ElevatedButton(
 
-                      onPressed: removeOverlay,
-                      child: Text('Close Overlay'),
-                    ),
-                  ],
+                        onPressed: removeOverlay,
+                        child: Text('Close Overlay'),
+                      ),
+                    ],
+                  ),
                 ),
               ),
             ),
