b7ed8e7f128c5fd6daba2d3292608d4fad1b5a29
David <arsovskidavid4@gmail.com>
2024-02-15 21:50:42 +0100

    fix na edit za menu

--- lib/forms/edit_menu_form.dart
+++ lib/forms/edit_menu_form.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:location_picker_flutter_map/location_picker_flutter_map.dart';
 import 'package:menu_craft/models/restaurant_category_model.dart';
+import 'package:menu_craft/models/restaurant_model.dart';
 import 'package:menu_craft/utils/location_services.dart';
 import 'package:menu_craft/widgets/menu/menu_input.dart';
 
@@ -19,16 +20,19 @@ class EditMenuForm extends StatefulWidget {
   final List<RestaurantCategoryModel> categories;
 
   final Function(RestaurantCategoryModel?) onCategorySelected;
+  final RestaurantModel restaurant;
 
-  const EditMenuForm(
-      {super.key,
-        required this.nameController,
-        required this.locationController,
-        required this.pickedImage,
-        required this.pickImage,
-        required this.onPressed,
-        required this.categories,
-        required this.onCategorySelected});
+  EditMenuForm({
+    super.key,
+    required this.nameController,
+    required this.locationController,
+    required this.pickedImage,
+    required this.pickImage,
+    required this.onPressed,
+    required this.categories,
+    required this.onCategorySelected,
+    required this.restaurant,
+  });
 
   @override
   State<EditMenuForm> createState() => _EditMenuFormState();
@@ -39,10 +43,31 @@ class _EditMenuFormState extends State<EditMenuForm> {
     setState(() {});
   }
 
+
   RestaurantCategoryModel? _selectedCategory;
 
   @override
   Widget build(BuildContext context) {
+
+    widget.nameController.value =
+        widget.nameController.value.text.isEmpty ? TextEditingValue(text: widget.restaurant.name) : widget.nameController.value;
+
+
+    _selectedCategory = widget.categories
+        .where((element) => element.name == widget.restaurant.category)
+        .firstOrNull;
+
+
+    widget.locationController?.data ??= PickedData(
+        LatLong(
+          widget.restaurant.latitude,
+          widget.restaurant.longitude,
+        ),
+        '',
+        {},
+      );
+
+
     return Expanded(
       child: Padding(
         padding: const EdgeInsets.only(bottom: 16.0),
@@ -88,12 +113,14 @@ class _EditMenuFormState extends State<EditMenuForm> {
                       onChanged: (RestaurantCategoryModel? newValue) {
                         setState(() {
                           _selectedCategory = newValue;
+                          widget.restaurant.category = newValue!.name;
                         });
                         widget.onCategorySelected(newValue);
                       },
                       items: [
                         const DropdownMenuItem<RestaurantCategoryModel>(
-                          value: null, // Set the value to null to represent no selection
+                          value: null,
+                          // Set the value to null to represent no selection
                           child: Text(
                             'Choose category',
                             style: TextStyle(
@@ -105,14 +132,14 @@ class _EditMenuFormState extends State<EditMenuForm> {
                         ...widget.categories
                             .map<DropdownMenuItem<RestaurantCategoryModel>>(
                                 (RestaurantCategoryModel value) {
-                              return DropdownMenuItem<RestaurantCategoryModel>(
-                                value: value,
-                                child: Text(
-                                  value.name,
-                                  style: const TextStyle(color: Colors.white),
-                                ), // Assuming CategoryModel has a 'name' property
-                              );
-                            }),
+                          return DropdownMenuItem<RestaurantCategoryModel>(
+                            value: value,
+                            child: Text(
+                              value.name,
+                              style: const TextStyle(color: Colors.white),
+                            ), // Assuming CategoryModel has a 'name' property
+                          );
+                        }),
                       ],
                     ),
                     const SizedBox(height: 20.0),
@@ -146,69 +173,69 @@ class _EditMenuFormState extends State<EditMenuForm> {
                     ),
                     widget.locationController?.data?.latLong != null
                         ? FutureBuilder(
-                        future: LocationService.getAddress(
-                          widget.locationController?.data?.latLong
-                              .latitude ??
-                              0.0,
-                          widget.locationController?.data?.latLong
-                              .longitude ??
-                              0.0,
-                        ),
-                        builder: (context, snapshot) {
-                          if (snapshot.connectionState ==
-                              ConnectionState.waiting) {
-                            return const Text("Finding location...",
-                                style: TextStyle(
-                                    color: Colors.white, fontSize: 20.0));
-                          }
-                          if (snapshot.hasError) {
-                            return Text('Error: ${snapshot.error}');
-                          }
-                          if (snapshot.hasData) {
-                            return Text(
-                              'Location: ${snapshot.data}',
-                              style: const TextStyle(
-                                color: Colors.white,
-                                fontSize: 20.0,
-                                overflow: TextOverflow.ellipsis,
-                              ),
-                              textAlign: TextAlign.center,
-                            );
-                          }
-                          return const SizedBox.shrink();
-                        })
+                            future: LocationService.getAddress(
+                              widget.locationController?.data?.latLong
+                                      .latitude ??
+                                  0.0,
+                              widget.locationController?.data?.latLong
+                                      .longitude ??
+                                  0.0,
+                            ),
+                            builder: (context, snapshot) {
+                              if (snapshot.connectionState ==
+                                  ConnectionState.waiting) {
+                                return const Text("Finding location...",
+                                    style: TextStyle(
+                                        color: Colors.white, fontSize: 20.0));
+                              }
+                              if (snapshot.hasError) {
+                                return Text('Error: ${snapshot.error}');
+                              }
+                              if (snapshot.hasData) {
+                                return Text(
+                                  'Location: ${snapshot.data}',
+                                  style: const TextStyle(
+                                    color: Colors.white,
+                                    fontSize: 20.0,
+                                    overflow: TextOverflow.ellipsis,
+                                  ),
+                                  textAlign: TextAlign.center,
+                                );
+                              }
+                              return const SizedBox.shrink();
+                            })
                         : const SizedBox.shrink(),
                     const SizedBox(height: 15.0),
                     widget.pickedImage != null
                         ? Column(
-                      children: [
-                        SizedBox(
-                            height: 200,
-                            child: Image.file(widget.pickedImage!)),
-                        const SizedBox(height: 5.0),
-                        TextButton.icon(
-                          onPressed: () async {
-                            await widget.pickImage(ImageSource.gallery);
-                          },
-                          icon: const Icon(Icons.image,
-                              color: Colors.white),
-                          label: const Text(
-                            'Pick Another Image',
-                            style: TextStyle(color: Colors.white),
-                          ),
-                        ),
-                      ],
-                    )
+                            children: [
+                              SizedBox(
+                                  height: 200,
+                                  child: Image.file(widget.pickedImage!)),
+                              const SizedBox(height: 5.0),
+                              TextButton.icon(
+                                onPressed: () async {
+                                  await widget.pickImage(ImageSource.gallery);
+                                },
+                                icon: const Icon(Icons.image,
+                                    color: Colors.white),
+                                label: const Text(
+                                  'Pick Another Image',
+                                  style: TextStyle(color: Colors.white),
+                                ),
+                              ),
+                            ],
+                          )
                         : TextButton.icon(
-                      onPressed: () async {
-                        await widget.pickImage(ImageSource.gallery);
-                      },
-                      icon: const Icon(Icons.image, color: Colors.white),
-                      label: const Text(
-                        'Pick an Image',
-                        style: TextStyle(color: Colors.white),
-                      ),
-                    ), //TODO: Loading screen
+                            onPressed: () async {
+                              await widget.pickImage(ImageSource.gallery);
+                            },
+                            icon: const Icon(Icons.image, color: Colors.white),
+                            label: const Text(
+                              'Pick an Image',
+                              style: TextStyle(color: Colors.white),
+                            ),
+                          ), //TODO: Loading screen
                   ],
                 ),
                 ElevatedButton(
@@ -221,8 +248,8 @@ class _EditMenuFormState extends State<EditMenuForm> {
                     minimumSize: const Size(double.infinity, 50),
                   ),
                   onPressed: widget.onPressed,
-                  child: const Text('Edit',
-                      style: TextStyle(color: Colors.white)),
+                  child:
+                      const Text('Edit', style: TextStyle(color: Colors.white)),
                 ),
               ],
             ),
